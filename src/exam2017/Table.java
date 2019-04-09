85406a01e8de8ac73d6ea1272cd58752db58bd7c
Lars Waage <46653859+larwaa@users.noreply.github.com>
2019-04-09 12:42:55 +0200

    added tableID

--- src/exam2017/Table.java
+++ src/exam2017/Table.java
@@ -2,14 +2,21 @@ package exam2017;
 
 public class Table {
 
+	private static int tableID = 1;
 	private final int seats;
+	private final int num;
 
 	public Table(int seats){
 		this.seats = seats;
+		this.num = tableID++;
 	}
 
 	public int getSeats(){
 		return seats;
+	}
+
+	public int getTableID(){
+		return tableID;
 	}
 
 }
